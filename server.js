65ff0cd8c77070a31dcf1f606858220f34f49ac7
Edgardo N. Angeles <87631824+Infin8Phy6@users.noreply.github.com>
2024-10-29 09:56:23 +0800

    v4

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+// Load environment variables from .env file
+require('dotenv').config(); 
+
 const { MongoClient } = require('mongodb');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use PORT from environment variables or default to 3000
 
-// Hardcoded MongoDB URI
-const uri = "mongodb+srv://angelesedgardo17:dNjeAKovMY0psOmU@students.mongodb.net/myusers?retryWrites=true&w=majority";
+// MongoDB URI from environment variables
+const uri = process.env.MONGODB_URI; 
 const client = new MongoClient(uri);
 
 async function connectToMongoDB() {
