1229da19d72779864d9e1495a5c50e1f84be321c
Edgardo N. Angeles <87631824+Infin8Phy6@users.noreply.github.com>
2024-10-29 09:25:21 +0800

    v1

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 require('dotenv').config(); // Load .env file
 
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 3000;
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+// Function to connect to MongoDB
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Connection error:", error);
+    }
+}
+
+// Call the MongoDB connection function
+connectToMongoDB();
 
 // Log the API_KEY to verify it's loaded
-console.log('API_KEY:', process.env.API_KEY);
+console.log('API_KEY:', process.env.MONGODB_USERNAME);
 
+// Define a basic route
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+    res.send('Hello, world!');
 });
 
+// Start the Express server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
